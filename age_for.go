5255a3ea33c4d9bd593d9f3d69629b06cc88b5fc
“rahulislamitibd” <itibdd@gmail.com>
2021-02-14 16:42:28 +0600

    forloop_forrange_forwhile

--- age_for.go
+++ age_for.go
@@ -1,14 +1,15 @@
+//for loop, for range, for while
 package main
 import(
 	"fmt"
 )
 func main(){
-//for loop 
-//1,2,3,4,5,6,7,8,9
-/*for i:=1; i<=100; i++ {
+/*for loop 
+1,2,3,4,5,6,7,8,9
+for i:=1; i<=100; i++ {
 fmt.Println(i)
 }
-//array string literals 
+array string literals 
 students := []string{"A","B","C"}
 for i, std := range students {
 fmt.Println(i, std)
