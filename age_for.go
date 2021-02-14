1329a5fe3b2d96ff5c44f8b581db79709c8ebcd0
“rahulislamitibd” <itibdd@gmail.com>
2021-02-14 16:25:40 +0600

    Switch_ifelse

---
+++ age_for.go
@@ -0,0 +1,25 @@
+package main
+import(
+	"fmt"
+)
+func main(){
+//for loop 
+//1,2,3,4,5,6,7,8,9
+/*for i:=1; i<=100; i++ {
+fmt.Println(i)
+}
+//array string literals 
+students := []string{"A","B","C"}
+for i, std := range students {
+fmt.Println(i, std)
+}*/
+var b int =15
+var c int =0
+for a:=0;a<10;a++{
+fmt.Printf("value of %d\n",a)
+}
+for c<b{
+c++
+fmt.Printf("value of c %d\n",c)
+}
+}
